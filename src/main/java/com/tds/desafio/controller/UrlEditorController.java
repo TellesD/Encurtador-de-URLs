744538b4d60440fb2c0fc7631c92013570a112ec
TellesD <victormteles@outlook.com>
2025-02-20 11:02:57 -0300

    firt commit

---
+++ src/main/java/com/tds/desafio/controller/UrlEditorController.java
@@ -0,0 +1,9 @@
+package com.tds.desafio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/url")
+public class UrlEditorController {
+}
