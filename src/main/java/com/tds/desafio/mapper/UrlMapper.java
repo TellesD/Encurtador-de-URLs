222921d3d2ec14272d9f0a435631589c10843c54
TellesD <victormteles@outlook.com>
2025-02-21 04:54:07 -0300

    ajuste mapper

--- src/main/java/com/tds/desafio/mapper/UrlMapper.java
+++ src/main/java/com/tds/desafio/mapper/UrlMapper.java
@@ -7,18 +7,18 @@ import java.time.LocalDateTime;
 public class UrlMapper {
 
     public static UrlModel toUrlModel(String originalUrl, String shortUrl) {
-        return new UrlModel(
-                null,
-                originalUrl,
-                shortUrl,
-                LocalDateTime.now(),
-                LocalDateTime.now(),
-                0
-        );
+        UrlModel urlModel = new UrlModel();
+        urlModel.setId(null);
+        urlModel.setUrl(originalUrl);
+        urlModel.setShortUrl(shortUrl);
+        urlModel.setUpdateDate(LocalDateTime.now());
+        urlModel.setCreateDate(LocalDateTime.now());
+        urlModel.setTotalView(0);
+        return urlModel;
     }
 
     public static UrlModel toUrlModel(UrlModel model) {
-        UrlModel urlModel = new UrlModel ();
+        UrlModel urlModel = new UrlModel();
         urlModel.setId(model.getId());
         urlModel.setUrl(model.getUrl());
         urlModel.setShortUrl(model.getShortUrl());
